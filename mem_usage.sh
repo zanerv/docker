64586c33a8ed4ddaba7ee35735bd7809af5164fd
zanerv <zanerv@gmail.com>
2018-06-09 08:49:38 +0200

    sort containers by mem and cpu

--- mem_usage.sh
+++ mem_usage.sh
@@ -9,17 +9,23 @@ if [ "$usage" -gt "$threshold" ]
 
 then
 
-storj=$(ps -eF --sort=-rss|grep json|head -n1|awk -F"[/.]" '{print $13}')
+storjtop3=$(ps -eF --sort=-rss|grep json|head -n3|awk -F"[/.]" '{print $13}')
+
+storj=$(docker stats $storjtop3 --no-stream --format "table {{.Name}}\t{{.CPUPerc}}" | sort -k 2 -h | grep -v CPU | head -1 | awk '{print $1}')
 
 /usr/bin/docker restart $storj
+
+     echo -e "The memory usage has reached $usage\% restarting:\n $storj\n Current usage: $current\%" | mail -s "Memory Usage Alert" ${EMAIL}
 
      if [ "$usage" -gt "$threshold2" ]
 
      then
 
-	 top3=$(ps -eF --sort=-rss|grep json|head -n3|awk -F"[/.]" '{print $13}')
+	 storjtop3=$(ps -eF --sort=-rss|grep json|head -n6|awk -F"[/.]" '{print $13}')
+	 
+	 storj=$(docker stats $storjtop3 --no-stream --format "table {{.Name}}\t{{.CPUPerc}}" | sort -k 2 -h | grep -v CPU | head -3 | awk '{print $1}')
 
-	 /usr/bin/docker restart $top3
+	 /usr/bin/docker restart storj
 
 	 current=$(($(($(free -m |awk 'NR==2 {print $3}') * 100)) / 16010))
 
