0b7e393748dc3c51b00b8c25ef4f35040d79fe88
zanerv <zanerv@gmail.com>
2020-02-19 22:55:18 +0100

    new bugs and features

--- s.sh
+++ s.sh
@@ -4,13 +4,12 @@ ip_list="/opt/ssh.ip"
 
 foe=$(grep -Po "(?<=from ).*(?= port)" /var/log/auth.log|sort -u|grep -v -E "$(cat /opt/ssh.ip|tr -d '\n')")
 
-if [ ! -z "$foe" ]
+if [ ! -z ${foe} ]
  then
   if [ ${#foe} > 15 ]
    then
-    log=$(echo $foe|sed 's/ /|/g')
-#    echo "$log"
+    log=$(echo ${foe}|sed 's/ /|/g')
   fi
-  echo -n "|$log" >> /opt/ssh.ip
-curl  --silent --output /dev/null -X POST -H "Content-Type: application/json" -d '{"title": "ssh","message": "https://whatismyipaddress.com/ip/'$(echo $log)'"}'  http://localhost:8123/api/services/notify/hass
+  echo -n "|${log}" >> /opt/ssh.ip
+curl  --silent --output /dev/null -X POST -H "Authorization: Bearer ${HASS_TOKEN}" -H "Content-Type: application/json" -d '{"title": "ssh","message": "https://whatismyipaddress.com/ip/'$(echo $log)'"}'  http://localhost:8123/api/services/notify/hass
 fi
